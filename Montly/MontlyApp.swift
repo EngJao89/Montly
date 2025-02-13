019a59acee34328ea52145ea27e5089c2779d7d5
EngJao89 <eng_joao@outlook.com>
2025-02-13 08:39:21 -0300

    Initial Commit

---
+++ Montly/MontlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MontlyApp.swift
+//  Montly
+//
+//  Created by João Ricardo Martins Ribeiro on 13/02/25.
+//
+
+import SwiftUI
+
+@main
+struct MontlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
